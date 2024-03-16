3a62a1765ba4eb40cf5b72bbd11d6ed437cdf55d
zyf <2530974390@qq.com>
2024-03-16 19:05:11 +0800

    assignment

---
+++ task0.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int func(long  m, long k, long* b) {
+    static long  c[999];
+    long  z, q, num=0;
+    for (z = 0; z <= 999-m; z++) {
+        for (q = 0; q < m; q++) {
+            num = num + *(b + q);
+        }
+        c[z] = k * m - num;
+    }
+    long temp, e = 0;
+    temp = c[0];
+    for (z = 1; z <= 999-m; z++) {
+        if (temp < c[z]) {
+            temp = c[z];
+            e = z;
+        }
+    }
+    if (temp <= 0) {
+        return -1;
+    }
+    else {
+        return e;
+    }
+}
+int main() {
+    long  n, m, k, i, e;
+    char s =0;
+    static long a[999];
+    printf("请输入");
+    scanf("%d", &n);
+    scanf("%d", &m);
+    scanf("%d", &k);
+    printf("请输入");
+    for (i = 0; i <= 999&&s!='\n'; i++) {
+        scanf("%d", &a[i]);
+        s= getchar();
+    }
+    long* b;
+    b = a;
+    e = func(m, k, b);
+    if (e == -1) {
+        printf("0   0");
+    }
+    else {
+        printf("%d   %d", e+1, e + m);
+    }
+}
