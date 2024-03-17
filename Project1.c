e643fdd06d8aa94ef1da54b6e54a256c59e0895e
zyf <2530974390@qq.com>
2024-03-17 19:40:53 +0800

    changes

---
+++ Project1.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.7.34031.279
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Project1", "Project1.vcxproj", "{310B8D18-2359-4C20-93C4-81259A726445}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{310B8D18-2359-4C20-93C4-81259A726445}.Debug|x64.ActiveCfg = Debug|x64
+		{310B8D18-2359-4C20-93C4-81259A726445}.Debug|x64.Build.0 = Debug|x64
+		{310B8D18-2359-4C20-93C4-81259A726445}.Debug|x86.ActiveCfg = Debug|Win32
+		{310B8D18-2359-4C20-93C4-81259A726445}.Debug|x86.Build.0 = Debug|Win32
+		{310B8D18-2359-4C20-93C4-81259A726445}.Release|x64.ActiveCfg = Release|x64
+		{310B8D18-2359-4C20-93C4-81259A726445}.Release|x64.Build.0 = Release|x64
+		{310B8D18-2359-4C20-93C4-81259A726445}.Release|x86.ActiveCfg = Release|Win32
+		{310B8D18-2359-4C20-93C4-81259A726445}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {A1F8B204-B49D-4650-BD2D-6BD0AAC67856}
+	EndGlobalSection
+EndGlobal
